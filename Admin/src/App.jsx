568f3963b36f97f98a46fc244f93e0a7afa9f959
rohan-bharekar-31 <128673201+rohan-bharekar-31@users.noreply.github.com>
2025-01-10 22:51:41 +0530

    Update App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import Sidebar from './components/sidebar/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar'
 import {Routes,Route} from 'react-router-dom'
 import Add from './pages/Add/Add'
 import Orders from './pages/Orders/Orders'
